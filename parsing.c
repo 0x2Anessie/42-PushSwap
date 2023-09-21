559482342ff5ae35b118a3eefc5d814b2a11b509
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-09-21 18:26:53 +0200

    Update parsing.c

--- parsing.c
+++ parsing.c
@@ -17,26 +17,30 @@ static int	check_digit(char *str)
 	return (1);
 }
 
-// Check que la chaîne soit dans la range traitée par le type int
+// Check que la chaîne soit dans la range traitée par le type int.
+// str > 11, forcément out of range
+// < 10 / égal à INT_MIN / négatif et len=10 forcément dans la range.
+// Si aucun des cas précédents, on compare chiffre par chiffre et si str >
+// ult_int, alors forcément out of range.
 static int	check_range(char *str)
 {
 	int		i;
 	int		j;
-	char	*ultmax;
+	char	*ult_int;
 
-	if (ft_strlen(str) > 11) // Forcément out of range
+	if (ft_strlen(str) > 11)
 		return (1);
 	else if (ft_strlen(str) < 10 || ft_strncmp(str, "-2147483648", 11) == 0
-		|| (str[0] == '-' && ft_strlen(str) == 10)) // Forcément dans la range
+		|| (str[0] == '-' && ft_strlen(str) == 10))
 		return (0);
 	i = 0;
 	j = 0;
-	ultmax = "-2147483647";
+	ult_int = "-2147483647";
 	if (str[0] != '-')
 		j = 1;
 	while (str[i] != '\0')
 	{
-		if (str[i] > ultmax[j]) // Si str[i] > ultmax[j] alors > INT_MAX
+		if (str[i] > ult_int[j])
 			return (1);
 		j++;
 		i++;
@@ -44,21 +48,24 @@ static int	check_range(char *str)
 	return (0);
 }
 
-// Check qu'il n'y a pas de digit en double dans les chaines du tab
+// Nb est la string actuellement traitée par ft_error, et argv[i] est la
+// string suivante dans le tableau. On incrémente i tant qu'aucune string ne
+// correspond à nb.
 static int	check_double(char **argv, int nb, int i)
 {
 	i++;
 	while (argv[i])
 	{
-		if (ft_atoi(argv[i]) == nb) // i comparé avec nb ((*argv)[i] - 1)
+		if (ft_atoi(argv[i]) == nb)
 			return (1);
 		i++;
 	}
 	return (0);
 }
 
-// Transforme argv en tableau de chaînes et vérifie a l'aide des fonctions
-// check que chaque chaîne du tableau ne comporte pas d'erreur.
+// Transforme av en tableau de chaînes grâce à "ft_split" si ac==2 
+// et/ou vérifie à l'aide des fonctions check_* que chaque chaîne du 
+// tableau ne comporte pas d'erreur.
 int	ft_error(int argc, char ***argv)
 {
 	int	i;
@@ -66,7 +73,7 @@ int	ft_error(int argc, char ***argv)
 	i = 1;
 	if (argc == 2)
 	{
-		*argv = ft_split((*argv)[1], ' '); // Crée un tableau de chaînes
+		*argv = ft_split((*argv)[1], ' ');
 		if (!(*argv))
 			return (0);
 		i = 0;
@@ -76,7 +83,7 @@ int	ft_error(int argc, char ***argv)
 		if (!check_digit((*argv)[i]) || check_range((*argv)[i]) || \
 			check_double((*argv), ft_atoi((*argv)[i]), i))
 		{
-			ft_putendl_fd("Error", 2); // Si une des erreurs est trouvée
+			ft_putendl_fd("Error", 2);
 			return (0);
 		}
 		i++;
