b50fb7bc57a69a1386209809bb59c87a77c48d58
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-10-03 12:25:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acatusse <acatusse@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nessie <nessie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/20 13:14:39 by acatusse          #+#    #+#             */
-/*   Updated: 2023/10/02 16:09:08 by acatusse         ###   ########.fr       */
+/*   Created: 2023/09/20 13:14:39 by nessie            #+#    #+#             */
+/*   Updated: 2023/10/03 12:24:18 by nessie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// Free les chaînes du tableau pour éviter les leaks potentiels causés par
-// "ft_split" dans la fonction "ft_error".
-static void	clean_string(char **argv)
+static void	clean_strings(char **argv)
 {
 	int	i;
 
@@ -25,8 +23,17 @@ static void	clean_string(char **argv)
 		free(argv[i]);
 }
 
-// On crée 2 piles initialisées à Null, fill la A avec "stack_init" et trie
-// son contenu avec "sort".
+/**
+ * @brief: Fonction main: Vérifie qu'il n'y ait pas d'erreurs dans la/les
+ * 		   chaînes passées en arg. Elle crée deux stack, les set a NULL puis
+ * 		   initialise la stack A avec stack_init. 
+ * 		   Le tri de la stack A s'effectura dans la fonction sort_a.
+ * 
+ * @warning: Pour éviter les fuites de mémoire, penser à clean le tableau (free)
+ * 			 et les chaînes qui le composent (clean_strings) quand argv == 2 car
+ * 			 la fonction ft_error utilise malloc dans ce cas là.
+ * 			 clean_stack: free les noeuds de la liste chaînée stack A.
+ */
 int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
@@ -36,7 +43,7 @@ int	main(int argc, char **argv)
 	{
 		if (argc == 2)
 		{
-			clean_string(argv);
+			clean_strings(argv);
 			free(argv);
 		}
 		return (0);
@@ -49,7 +56,7 @@ int	main(int argc, char **argv)
 	sort_a(&stack_a, &stack_b);
 	if (argc == 2)
 	{
-		clean_string(argv);
+		clean_strings(argv);
 		free(argv);
 	}
 	clean_stack(&stack_a);
