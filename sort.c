4bc780b21a4ee42308cb613896674f9f890e226e
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-10-03 19:43:57 +0200

    Update sort.c

--- sort.c
+++ sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sort.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acatusse <acatusse@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nessie <nessie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/20 13:15:26 by acatusse          #+#    #+#             */
-/*   Updated: 2023/10/02 15:31:21 by acatusse         ###   ########.fr       */
+/*   Created: 2023/09/20 13:15:26 by nessie            #+#    #+#             */
+/*   Updated: 2023/10/03 19:36:20 by nessie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,33 +27,18 @@ int	stacklen(t_stack *stack)
 	return (len);
 }
 
-// Verifie si la pile est deja triee dans l'ordre decroissant
-// Pour chaque noeud elle verifie que sa valeur soit inferieure a la
-// valeur du noeud suivant.
-int	checkbackwards(t_stack **a)
-{
-	t_stack	*stack;
-
-	stack = *a;
-	while (stack->p)
-	{
-		if (stack->nbr < stack->p->nbr)
-			return (0);
-		stack = stack->p;
-	}
-	return (1);
-}
-
-// Une fois qu'on a le nombre d'elements dans la pile, et qu'on a check les
-// erreurs comme un seul arg ou si la pile est triee par ordre decroissant.
-// En foction du nombre d'arguments, la pile a trier est envoyee dans
-// les differentes fonctions de tri.
+/**
+ * @brief Fonction qui dirige les différentes stacks vers les fonctions de tri
+ * 		  en fonction du nombre d'arguments.
+ * 		  stacklen: calcule la taille de la stack A.
+ * 		  checkbackwards: vérifie que la stack A ne soit pas triée à l'envers.
+ */
 void	sort_a(t_stack **a, t_stack **b)
 {
 	int	len;
 
 	len = stacklen(*a);
-	if (len <= 1 || checkbackwards(a))
+	if (len <= 1)
 		return ;
 	else if (len == 2)
 		sa(a);
