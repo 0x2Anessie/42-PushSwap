d5942e2d89e7338e0f53381ac5546347469aaaf5
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-10-09 12:30:54 +0200

    Update push.c

--- push.c
+++ push.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acatusse <acatusse@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nessie <nessie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/20 13:14:49 by acatusse          #+#    #+#             */
-/*   Updated: 2023/10/02 15:35:52 by acatusse         ###   ########.fr       */
+/*   Created: 2023/09/20 13:14:49 by nessie            #+#    #+#             */
+/*   Updated: 2023/10/02 15:35:52 by nessie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	push(t_stack **stack_src, t_stack **stack_dest)
 {
-	t_stack	*third_last;
+	t_stack	*first_node;
 
 	if (stacklen(*stack_src) == 0)
 		ft_putendl_fd("No value in source stack", 1);
@@ -23,23 +23,27 @@ void	push(t_stack **stack_src, t_stack **stack_dest)
 		*stack_src = NULL;
 	else
 	{
-		third_last = *stack_src;
-		while (third_last->p->p)
-			third_last = third_last->p;
-		third_last->p = NULL;
+		first_node = *stack_src;
+		while (first_node->p->p)
+			first_node = first_node->p;
+		first_node->p = NULL;
 	}
 }
 
-// Prend le premier élément au sommet de b et le met sur a.
-// Ne fait rien si b est vide.
+/**
+ * @brief Prend le premier élément au sommet de B et le met sur A.
+ * 		  Ne fait rien si b est vide.
+ */
 void	pa(t_stack **a, t_stack **b)
 {
 	push(b, a);
 	ft_putendl_fd("pa", 1);
 }
 
-// Prend le premier élément au sommet de a et le met sur b.
-// Ne fait rien si a est vide.
+/**
+ * @brief Prend le premier élément au sommet de A et le met sur B.
+ * 		  Ne fait rien si a est vide.
+ */
 void	pb(t_stack **b, t_stack **a)
 {
 	push(a, b);
